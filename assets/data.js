59f393386842fae739d215951cc849da80d6f2f7
jamieilagan <ilaganjanmaridel@gmail.com>
2025-11-24 19:07:49 +0800

    Updated responsiveness

---
+++ assets/data.js
@@ -0,0 +1,92 @@
+var artworks = [
+    {
+        title: "Pilipinas Not for Sale",
+        artist: "Aba Dalena",
+        description: "Poster Print on Wood Panel",
+        image: "assets/images/A1.jpg"
+    },
+    {
+        title: "A Never-Ending Imprisoned Childhood",
+        artist: "Argee Dacuyan",
+        description: "Acrylic, Oil, and Wood on Torn Canvas Mounted on Wood Panel",
+        image: "assets/images/A2.jpg"
+    },
+    {
+        title: "Robber Cut",
+        artist: "Bryan Barrios",
+        description: "Acrylic, Spray Paint, Offset Ink on Canvas",
+        image: "assets/images/A3.jpg"
+    },
+    {
+        title: "The Pig",
+        artist: "Carlo De Laza",
+        description: "Acrylic on Canvas",
+        image: "assets/images/A4.jpg"
+    },
+    {
+        title: "Velocity",
+        artist: "CJ Tañedo",
+        description: "Oil and Acrylic on Wood Panel",
+        image: "assets/images/A5.jpg"
+    },
+    {
+        title: "Sa Magkabilang Dako",
+        artist: "John Paul Antido",
+        description: "Acrylic on Wood Panel",
+        image: "assets/images/A6.jpg"
+    },
+    {
+        title: "Lakbay-Buhayt",
+        artist: "Jonathan Joven",
+        description: "Graphite and Acrylic on Repurposed Tracing Paper on Wood Panel",
+        image: "assets/images/A7.jpg"
+    },
+    {
+        title: "Pedal Power and other Related Matters",
+        artist: "Katti Sta Ana",
+        description: "Acrylic and Ink on Wood Panel",
+        image: "assets/images/A8.jpg"
+    },
+    {
+        title: "Ups and Downs",
+        artist: "Kendall Collindon",
+        description: "Acrylic on Wood Panel",
+        image: "assets/images/A9.jpg"
+    },
+    {
+        title: "Cost and Effect",
+        artist: "KR Rodgers",
+        description: "Acrylic on Canvas",
+        image: "assets/images/A10.jpg"
+    },
+    {
+        title: "Sigad",
+        artist: "Lorebert Mralita",
+        description: "Acrylic and Oil on Canvas",
+        image: "assets/images/A11.jpg"
+    },
+    {
+        title: "Arangkada I and II",
+        artist: "Pedro Felix Garcia",
+        description: "Oil on Canvas",
+        image: "assets/images/A12.jpg"
+    },
+    {
+        title: "Two Sides of Insanity",
+        artist: "Rai Cruz",
+        description: "Acrylic on Wood Panel",
+        image: "assets/images/A13.jpg"
+    },
+    {
+        title: "Politiko-Payaso",
+        artist: "Reynold Dela Cruz",
+        description: "Acrylic, Spray Paint, Offset Ink on Canvas",
+        image: "assets/images/A14.jpg"
+    },
+    {
+        title: "Lubog",
+        artist: "Rey Bollozos",
+        description: "Ink, Oil, Joomchi on Wood Panel",
+        image: "assets/images/A15.jpg"
+    }
+];
