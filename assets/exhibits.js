2c8baf596fe7516bd192e99910d34ccdfa5ae96a
jamieilagan <ilaganjanmaridel@gmail.com>
2025-11-26 00:46:04 +0800

    fixed cards and file construction

---
+++ assets/exhibits.js
@@ -0,0 +1,17 @@
+var exhibits = [
+    {
+        image: "assets/images/siete.jpg",
+        title: "Siete Palabras",
+        link: "https://www.eskinitaartgallery.com/siete-palabras"
+    },
+    {
+        image: "assets/images/hisoka.jpg",
+        title: "Akosi Hisoka",
+        link: "https://www.eskinitaartgallery.com/akosihisoka"
+    },
+    {
+        image: "assets/images/unio.jpg",
+        title: "Unio Mystica",
+        link: "https://www.eskinitaartgallery.com/unio-mystica"
+    }
+];
